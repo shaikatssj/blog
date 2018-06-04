9ebfdeace602ae05252dbe679feaa03cf44c5e57
Ronnie Ajax <39485992+shaikatssj@users.noreply.github.com>
2018-06-03 22:40:45 -0700

    Update and rename urlloads.js to urloads.js

--- urloads.js
+++ urloads.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $.ajax({
-    url: "https://cdn.rawgit.com/shaikatssj/scodec-cdn/master/index.html", 
+    url: urt, 
     dataType: 'text', 
     success: function(data) { 
       $("#sentences").html(data);
